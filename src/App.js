92b9e4d8b500437eee1547cdb389775e8ce40122
yykdev <assavictory@gmail.com>
2020-08-12 11:59:14 +0900

    useState 기본 카운터 구현

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    return (
+        <div className="App">
+            <h1>Hello {count}</h1>
+            <h2>Start editing to see some magic happen!</h2>
+            <button onClick={() => setCount(count + 1)}>Icreament</button>
+            <button onClick={() => setCount(count - 1)}>Decreament</button>
+        </div>
+    );
 }
 
 export default App;
