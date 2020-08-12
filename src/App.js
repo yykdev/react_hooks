bdd755cfd3e2c583c97cb39fc7a7c2ba03db1c94
yykdev <assavictory@gmail.com>
2020-08-12 15:11:21 +0900

    커스텀 hooks : useTabs

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 import UseStateComponent from "./hooksComponent/useStateComponent";
 import UseInputComponent from "./hooksComponent/useInputComponent";
+import UseTabsComponent from "./hooksComponent/useTabsComponent";
 
 function App() {
     return (
         <div className="App">
             <UseStateComponent />
             <UseInputComponent />
+            <UseTabsComponent />
         </div>
     );
 }
